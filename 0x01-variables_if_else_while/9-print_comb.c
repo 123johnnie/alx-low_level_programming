df032e5fa781e50480ae8fa444b72db29b02873c
123johnnie <githuaj21'gmail.com>
2022-12-13 07:06:18 -0800

    Task 10

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,16 +2,18 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return; Always 0 (Success)
  */
 
 int main(void)
 {
 	int a;
 
-	for ( a = 0; a < 100; a++) {
+	for (a = 0; a < 10; a++)
+	{
 		putchar(a + '0');
-		if (a < 99) {
+		if (a < 9)
+		{
 			putchar(',');
 			putchar(' ');
 		}
