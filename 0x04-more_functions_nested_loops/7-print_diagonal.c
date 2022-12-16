4c0680028d28664212634bba3a17a5c4ad790fa9
123johnnie <githuaj21'gmail.com>
2022-12-16 04:19:05 -0800

    8-print_square.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_diagonal - function that draws a diagonal line on the terminal
+ * main: print_diagonal - function that draws a diagonal line on the terminal
  * @n: input number
  * Return: diagonal
  */
