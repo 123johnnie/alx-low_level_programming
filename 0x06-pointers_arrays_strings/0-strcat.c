f6dd5b0e163731b289709bfd9f501f997c0d62f4
123johnnie <githuaj21'gmail.com>
2022-12-21 04:38:59 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,25 +1,23 @@
 #include "main.h"
 
 /**
- * _strcat - function that concatenates
- * two things.
+ * _strcat - concatenates the string pointed to by @src to
+ * the end of the string pointed by @dest
+ * @dest: string that will be appended
+ * @src: string to be concatenated upon
  *
- * @dest: pointer to destination input
- * @src: pointer to source input
- *
- * Return: pointer to resulting string @dest
+ * Return: returns pointer to @dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-int c, c2;
-c = 0;
-/* find the size of dest array*/
-while (dest[c])
-c++
-/*iterate through each src array value without the null byte*/
-for (c2 = 0; src[c2] ; c2++)
-/*append src [c2] to dest [c] while overwritting the null byte in dest*/
-dest[c++] = src[c2];
-return (dest);
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }
