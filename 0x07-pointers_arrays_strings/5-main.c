e140375504d50c7347b0e77fde266c452db66795
123johnnie <githuaj21'gmail.com>
2023-01-04 07:00:12 -0800

    Task 5

--- 0x07-pointers_arrays_strings/5-main.c
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -8,12 +8,13 @@
  */
 
 int main(void)
-{	   
-       	char *s = "hello, world";        
-	char *f = "world";	    
+{
+	char *s = "hello, world";
+	char *f = "world";
 	char *t;
-	t = _strstr(s, f);		
-	printf("%s\n", t);		        
+
+	t = _strstr(s, f);
+	printf("%s\n", t);
 	return (0);
 
 }
